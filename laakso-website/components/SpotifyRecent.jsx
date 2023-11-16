bf6e57afa91cd82a0e86aa5ddf72834c4b550462
Laakso, Colin <33666686+laxattax99@users.noreply.github.com>
2023-11-16 17:59:45 -0500

    Added loading state to spotify

--- laakso-website/components/SpotifyRecent.jsx
+++ laakso-website/components/SpotifyRecent.jsx
@@ -4,26 +4,37 @@ import axios from "axios";
 import Link from "next/link";
 const SpotifyRecent = () => {
   const [recentSong, setRecentSong] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const getRecentlyPlayed = async () => {
-      const response = await axios.get("http://localhost:3001/recently-played");
-      const recent = response.data;
-      setRecentSong(recent);
+    const getRecentlyPlayed = () => {
+      axios.get("http://localhost:3001/recently-played").then((response) => {
+        const recent = response.data;
+        setRecentSong(recent);
+        setLoading(false);
+      });
     };
     getRecentlyPlayed();
   }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500"></div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col items-start m-4 p-4">
       <h2 className="text-4xl font-bold mb-4">Listening to:</h2>
       <Image
         className="shadow-lg"
-        src={recentSong.Imageurl}
+        src={recentSong.ImageUrl}
         alt="trackimage"
         width="300"
         height="300"
-        ></Image>
+      />
       <p className="text-lg mt-4 p-1">{recentSong.Trackname}</p>
       <p className="text-lg mt-2 p-1">{recentSong.Artistname}</p>
     </div>
